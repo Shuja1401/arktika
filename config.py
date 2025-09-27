f6363570a893fc2ce00d230afd14fda8fcf8e4ef
Shuja1401 <asrar.shuja@gmail.com>
2025-09-27 18:29:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+# config.py (safe for Git)
+PROJECT_ID = "arktika-471613"
+SECRETS = {
+    "GITHUB_TOKEN": "GITHUB_TOKEN",
+    "TWITTER_BEARER_TOKEN": "TWITTER_BEARER_TOKEN",
+}
+
+# API endpoints
+GITHUB_API_URL = "https://api.github.com"
+TWITTER_API_URL = "https://api.twitter.com/2"
+
+# political party account usernames
+PARTY_ACCOUNTS = {
+    "INC_Bihar": {
+        "x": "incbihar",
+        "fb": "incbihar",
+        "insta": "inc_bihar",
+        "party": "INC",
+        "designation": "State party"
+    },
+    "BJP_Bihar": {
+        "x": "bjp4bihar",
+        "fb": "bjp4bihar",
+        "insta": "bjp4bihar",
+        "party": "BJP",
+        "designation": "State party"
+    },
+    "RJD": {
+        "x": "rjdforindia",
+        "fb": "rjdparty",
+        "insta": "rjd4bihar_official",
+        "party": "RJD",
+        "designation": "State party"
+    },
+    "JDU": {
+        "x": "jduonline",
+        "fb": "jduonline",
+        "insta": "jdu.online",
+        "party": "JDU",
+        "designation": "State party"
+    }
+}
